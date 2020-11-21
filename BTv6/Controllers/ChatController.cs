7d894c9f0d87dd88b7637d7ec4432bf95af5c00d
imranalways <imran13854@gmail.com>
2020-11-21 12:30:51 +0600

    Chat Modified

---
+++ BTv6/Controllers/ChatController.cs
@@ -0,0 +1,52 @@
+﻿using BTv6.Models;
+using BTv6.Repositories.CommonRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTv6.Controllers
+{
+    public class ChatController : Controller
+    {
+        ChatRepository chatrepo = new ChatRepository();
+
+        public ActionResult Index()
+        {
+
+            if (Session["SID"] != null)
+            {
+                TempData["userId"] = (string)Session["LID"];
+                ViewData["receiver"] = chatrepo.GetAllByReceiverId((string)Session["LID"]);
+                ViewData["sender"] = chatrepo.GetAllBySenderId((string)Session["LID"]);
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Index(chat ct)
+        {
+            if (Session["SID"] != null)
+            {
+                chat chatToInsert = new chat();
+                chatToInsert.RECEIVER = ct.RECEIVER;
+                chatToInsert.SENDER = (string)Session["LID"];
+                chatToInsert.SUB = ct.SUB;
+                chatToInsert.TEXT = ct.TEXT;
+                chatToInsert.STATUS = 0;
+                chatrepo.Insert(chatToInsert);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+    }
+}
---
+++ BTv6/Repositories/CommonRepositories/ChatRepository.cs
@@ -0,0 +1,21 @@
+﻿using BTv6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTv6.Repositories.CommonRepositories
+{
+    public class ChatRepository : Repository<chat>
+    {
+        public List<chat> GetAllByReceiverId(string id)
+        {
+            return this.context.Set<chat>().Where(x => x.RECEIVER == id).ToList();
+        }
+
+        public List<chat> GetAllBySenderId(string id)
+        {
+            return this.context.Set<chat>().Where(x => x.SENDER == id).ToList();
+        }
+    }
+}
