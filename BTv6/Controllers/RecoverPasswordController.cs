3e1079a06d37726ef071d11b2a636e3abeed74e8
hasib999 <45915540+hasib999@users.noreply.github.com>
2020-11-22 01:41:40 +0600

    Reset password Done

--- BTv6/Controllers/RecoverPasswordController.cs
+++ BTv6/Controllers/RecoverPasswordController.cs
@@ -1,17 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using BTv6.Models;
+using BTv6.Repositories.CommonRepositories;
 
 namespace BTv6.Controllers
 {
     public class RecoverPasswordController : Controller
     {
         // GET: RecoverPassword
+
+        /*public bool IsEmailExist(string emailID)
+        {
+            using (BusinessToolDBEntities dc = new BusinessToolDBEntities())
+            {
+                var v = dc.employees.Where(a => a.E_MAIL == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }*/
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID, string emailFor = "ResetPassword")
+        {
+            var verifyUrl = "/RecoverPassword/" + emailFor + "/";
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("businesstoolsv5@gmail.com", "Dotnet Awesome");
+            var toEmail = new MailAddress(emailID);
+            var fromEmailPassword = "business@tools";
+            string subject = "Reset Password";
+
+            string body = "Hi,<br/><br/>We got Request for Reset your password.Please Click on below link" +
+                "<br/><br/><a href=" + link + ">Reset Password Link</a>";
+
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+        }
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(string EmailID)
+        {
+            //string message = "";
+            //bool status = false;
+            using (BusinessToolDBEntities dc = new BusinessToolDBEntities())
+            {
+                var account = dc.employees.Where(a => a.E_MAIL == EmailID).FirstOrDefault();
+                if(account.DID!=5)
+                {
+                    if (account != null)
+                    {
+                        //Send Mail
+                        Session["email"] = account.E_MAIL;
+                        SendVerificationLinkEmail(account.E_MAIL, "ResetPassword");
+
+                    }
+                    else
+                    {
+                        TempData["message"] = "Account Not Found";
+                        return View();
+                    }
+                }
+                else
+                {
+                    var account1 = dc.customers.Where(a => a.email == EmailID).FirstOrDefault();
+                    if(account1!=null)
+                    {
+                        Session["email"] = account1.email;
+                        SendVerificationLinkEmail(account1.email, "ResetPassword");
+                    }
+                    else
+                    {
+                        TempData["message"] = "Account Not Found";
+                        return View();
+                    }
+                }
+                
+            }
+            return RedirectToAction("Index","Login");
+        }
+        [HttpGet]
+        public ActionResult ResetPassword()
+        {
+            if (Session["email"]!=null)
+            {
+                ViewData["email"] = Session["email"];
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index","Login");
+            }
+            
+        }
+        [HttpPost]
+        public ActionResult ResetPassword(string EmailID)
+        {
+            using (BusinessToolDBEntities dc = new BusinessToolDBEntities())
+            {
+                var v = dc.employees.Where(a => a.E_MAIL == EmailID).FirstOrDefault();
+                if(v.E_MAIL==Request["EmailID"])
+                {
+                    if(Request["newpass"]==Request["connewpass"])
+                    {
+                        var userFromDB = dc.log_in.Where(a => a.LID == v.EmpID).FirstOrDefault();
+                        userFromDB.PASS = Request["connewpass"];
+                        dc.Entry(userFromDB).State = EntityState.Modified;
+                        dc.SaveChanges();
+                        Session.Clear();
+                    }
+                    else
+                    {
+                        TempData["error"] = "Password Doesn't Match";
+                        return RedirectToAction("ResetPassword", "RecoverPassword");
+                    }
+                    TempData["suc"] = "Password Recovered";
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+                
+            }
+            
+        }
+
     }
 }
