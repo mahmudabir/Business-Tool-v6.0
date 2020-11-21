1788125d800140dba97206455929fddd48dee073
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2020-11-22 00:02:57 +0600

    ADMIN

---
+++ BTv6/Repositories/AdminRepositories/NoticeRepository.cs
@@ -0,0 +1,21 @@
+﻿using BTv6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTv6.Repositories.AdminRepositories
+{
+    public class NoticeRepository : Repository<notice>
+    {
+        public notice GetByID(int id)
+        {
+            return context.notices.Where(x => x.noticeID == id).FirstOrDefault();
+        }
+        public void DeleteNoticeByID(int id)
+        {
+            this.context.notices.Remove(GetByID(id));
+            this.context.SaveChanges();
+        }
+    }
+}
